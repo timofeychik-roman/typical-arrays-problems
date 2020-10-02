e054e0c3271b9d4841c67855779be816bb4c5df1
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-02 11:15:00 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minimum = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < minimum)
+      minimum = array[i];
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] > minimum)
+      maximum = array[i];
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let average = 0;
+  for (let i = 0; i < array.length; i++)
+    average += array[i];
+  average = average / array.length;
+  return average;
 }
